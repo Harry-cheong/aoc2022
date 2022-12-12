82681a8bfd77f4de831cc2142fe1ec6532aaeaa5
Harry <harrycheongjunkai@gmail.com>
2022-12-12 17:26:10 +0800

    s01-03dec

---
+++ Lesson Notes/LearnCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Calculator
+
+int main() {
+    int x, y; 
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    cout << x + y;
+    return 0;
+}
